cbebc909117e784f03c1b6331f21f241a82e3f70
Funatik <funatic@MacBookAir.local>
2019-09-29 17:56:17 +0200

    Updates

---
+++ precache-manifest.37bcf964b8df62f671ad582664d86290.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26cd6f40fe5d3d5395027be41d054173",
+    "url": "/Made_in_Nizar/index.html"
+  },
+  {
+    "revision": "db3ba332a79c2b775246",
+    "url": "/Made_in_Nizar/static/css/main.313e8620.chunk.css"
+  },
+  {
+    "revision": "c9a2dd272ea48465ff76",
+    "url": "/Made_in_Nizar/static/js/2.b95d53ae.chunk.js"
+  },
+  {
+    "revision": "db3ba332a79c2b775246",
+    "url": "/Made_in_Nizar/static/js/main.650ba072.chunk.js"
+  },
+  {
+    "revision": "01436a1fbfa1976b3d83",
+    "url": "/Made_in_Nizar/static/js/runtime-main.77f8532e.js"
+  }
+]);
